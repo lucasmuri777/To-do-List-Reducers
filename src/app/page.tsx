75b0c82c443e5d8a6a2c5c98ee5af0a171ec8d65
lucasmuri777 <62767967+lucasmuri777@users.noreply.github.com>
2023-08-07 13:57:58 -0300

    Primeiro commit para o git

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,164 @@
-import Image from 'next/image'
+'use client'
+
+import { listReducer } from '@/reducers/listReducer';
+import { Item } from '@/types/item';
+import {useState, useReducer, useEffect} from 'react'
+
+import {BsTrashFill} from 'react-icons/bs'
+import {AiFillEdit} from 'react-icons/ai'
 
 export default function Home() {
+  const [list, dispatch] = useReducer(listReducer, []);
+  const [addField, setAddField] = useState('');
+  const [newText, setNewText] = useState('');
+  const [showEdit, setShowEdit] = useState(false)
+  const [showId, setShowId] = useState(0)
+
+
+  useEffect(()=>{
+    let lista: Item[] | null | string | void = localStorage.getItem('Lista')
+    if(lista == null){
+      lista = localStorage.setItem('Lista', '')
+      return
+    }else if(lista != '' && lista != null){
+      console.log(lista)
+      let newList: Item[] = JSON.parse(lista)
+      newList.forEach((item)=>{
+        dispatch({
+          type: 'add',
+          payload:{
+            text: item.text
+          }
+        })
+      })
+    }
+    
+  },[])
+
+  useEffect(()=>{
+    let lista: Item[] | null | string | void = localStorage.getItem('Lista')
+    if(lista || lista == ''){
+      lista = localStorage.setItem('Lista', JSON.stringify(list))
+    }
+  },[list])
+
+  const handleAddButton = () =>{
+    if(addField.trim().length < 5){
+      alert('Seu texto tem que ter mais de 4 caracteres')
+      return false
+    }
+    dispatch({
+      type: 'add',
+      payload:{
+        text: addField.trim()
+      }
+    })
+    setAddField('')
+  }
+
+  const handleDoneCheckBox = (id: number) =>{
+    dispatch({
+      type: 'toggleDone',
+      payload:{
+        id
+      }
+    })
+  }
+
+  const handleShowEdit = (text: string, id: number) =>{
+    setNewText(text)
+    setShowEdit(true)
+    setShowId(id)
+  }
+  const handleEdit = (id: number) =>{
+    if(newText.trim().length < 5){
+      setShowEdit(false)
+      return false
+    }
+    dispatch({
+      type: 'editText',
+      payload:{
+        id,
+        newText: newText
+      }
+    })
+    setShowEdit(false)
+  }
+  const handleRemove = (id: number) =>{
+    if(!window.confirm('Tem certeza que deseja excluir'))return false
+    dispatch({
+      type: 'remove',
+      payload: {
+        id
+      }
+    })
+  }
+  
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className='container mx-auto'>
+     
+      <h1 className='text-center text-4xl my-4'>Lista de Tarefas</h1>
+      <div className='max-w-2xl mx-auto flex rounded-md bg-gray-900 border border-gray-400 p-4 my-4'>
+        <input 
+          placeholder='Digite um item'
+          type='text' 
+          className='outline-none flex-1 rounded-md border border-white p-3 bg-transparent text-white' 
+          value={addField}
+          onChange={e=>setAddField(e.target.value)}
         />
+        <button 
+          className='p-4'
+          onClick={handleAddButton}
+        >
+          ADICIONAR
+        </button>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <ul className='max-w-2xl mx-auto'>
+        {list.map(item=>(
+          <li
+            className='flex items-center p-3 my-3 border-b border-gray-700' 
+            key={item.id}
+          >
+            <input 
+              type='checkbox'
+              className='w-6 h-6 mr-4'
+              checked={item.done}
+              onClick={() => handleDoneCheckBox(item.id)}
+              onChange={()=>{}}
+            />
+            {showEdit && showId == item.id ? (
+              <input 
+                type='text' 
+                placeholder='Editando...'
+                value={newText}
+                onChange={e => setNewText(e.target.value)}
+                className='flex-1 text-lg bg-transparent border rounded-md px-3 text-white' 
+                />
+            ):(
+              <p className='flex-1 text-lg'>{item.text}</p>
+            )}
+            
+            <button onClick={showEdit ? ()=>handleEdit(item.id) as any : ()=>handleShowEdit(item.text, item.id) as any} className='mx-4 hover:text-gray-500'>
+              {showEdit && showId == item.id ? (
+                <p>Salvar</p>
+              ):(
+                <AiFillEdit size={25} />
+              )}
+             
+              
+              </button>
+            <button 
+              onClick={()=>handleRemove(item.id)}
+              className='mx-4 hover:text-gray-500'
+            >
+              <BsTrashFill size={21}/>
+            </button>
+          </li>
+        ))}
+      </ul>
+      
+    
+    </div>
   )
 }
